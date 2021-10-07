c61ffee5e8fbe94119947f452c2416e9a09ea615
magdalena.traber <magdalena.traber@gmx.net>
2021-10-07 11:09:48 +0200

    implement part 1: InMemoryRepository

---
+++ 01_repository/src/main/java/at/campus02/dbp2/repository/Customer.java
@@ -0,0 +1,38 @@
+package at.campus02.dbp2.repository;
+
+public class Customer{
+    private  String email, lastname, firstname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "email='" + email + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                '}';
+    }
+}
